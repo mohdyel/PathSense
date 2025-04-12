468583122226b39c36969186d022f34fc56efaef
Mohammed Alduk <officialmohdyel@gmail.com>
2025-04-12 17:17:15 +0300

    Initial commit

---
+++ lib/fitness_app/models/meals_list_data.dart
@@ -0,0 +1,52 @@
+class MealsListData {
+  MealsListData({
+    this.imagePath = '',
+    this.titleTxt = '',
+    this.startColor = '',
+    this.endColor = '',
+    this.meals,
+    this.kacl = 0,
+  });
+
+  String imagePath;
+  String titleTxt;
+  String startColor;
+  String endColor;
+  List<String>? meals;
+  int kacl;
+
+  static List<MealsListData> tabIconsList = <MealsListData>[
+    MealsListData(
+      imagePath: 'assets/fitness_app/breakfast.png',
+      titleTxt: 'PathGuide Navigation',
+      kacl: 0,
+      meals: <String>[''],
+      startColor: '#FA7D82',
+      endColor: '#FFB295',
+    ),
+    MealsListData(
+      imagePath: 'assets/fitness_app/lunch.png',
+      titleTxt: 'Upcoming Events',
+      kacl: 0,
+      meals: <String>[''],
+      startColor: '#738AE6',
+      endColor: '#5C5EDD',
+    ),
+    MealsListData(
+      imagePath: 'assets/fitness_app/snack.png',
+      titleTxt: 'My Passes',
+      kacl: 0,
+      meals: <String>[''],
+      startColor: '#FE95B6',
+      endColor: '#FF5287',
+    ),
+    MealsListData(
+      imagePath: 'assets/fitness_app/dinner.png',
+      titleTxt: 'Frequently Asked Questions',
+      kacl: 0,
+      meals: <String>[''],
+      startColor: '#6F72CA',
+      endColor: '#1E1466',
+    ),
+  ];
+}
---
+++ lib/fitness_app/ui_view/workout_view.dart
@@ -0,0 +1,5 @@
+import 'package:best_flutter_ui_templates/main.dart';
+import 'package:flutter/material.dart';
+import '../fitness_app_theme.dart';
+
+
